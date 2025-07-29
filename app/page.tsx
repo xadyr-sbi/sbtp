731db4869de6622b0176b2bd6c5025d4dd95517f
xadyr <xadyran@gmail.com>
2025-07-29 14:42:52 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
 import Sidebar from "@/components/Sidebar"
 import MainContent from "@/components/MainContent"
+import Footer from "@/components/Footer"
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
       <main className="flex flex-1 max-w-7xl mx-auto px-4 lg:px-6 py-6 gap-6">
-        {/* Konten Utama */}
-        <div className="flex-1">
-          <MainContent />
-        </div>
-        {/* Sidebar */}
-        <div className="w-72 hidden lg:block">
-          <Sidebar />
-        </div>
+        <Sidebar />
+        <MainContent />
       </main>
       <Footer />
     </div>
