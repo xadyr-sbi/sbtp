c1c66a8fa4e42bc51c85f92447cf3aebeb0ef463
xadyr <xadyran@gmail.com>
2025-07-29 10:35:56 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react"
 import { createClient } from "@supabase/supabase-js"
 import Link from "next/link"
 
-// Langsung pakai URL & KEY dari kamu
 const supabase = createClient(
   "https://ftjtjwyxuqrbrhjdahqk.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0anRqd3l4dXFyYnJoamRhaHFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTA4NzMsImV4cCI6MjA2OTI2Njg3M30.VqOgC2CoWDWwYChMQxZkW0an4ZNIQOh1ywmIfmlgNFY"
@@ -11,30 +10,33 @@ const supabase = createClient(
 
 export default function Navbar() {
   const [title, setTitle] = useState("")
+  const [tagline, setTagline] = useState("")
 
   useEffect(() => {
-    const fetchTitle = async () => {
+    const fetchHeader = async () => {
       const { data, error } = await supabase
         .from("pages")
-        .select("title")
+        .select("title, tagline")
         .eq("slug", "home")
         .single()
 
       if (!error && data) {
         setTitle(data.title)
+        setTagline(data.tagline)
       } else {
         setTitle("SBTP-FSBI")
+        setTagline("Berani, Jujur, Cerdas, Militan")
         console.error("Supabase error:", error)
       }
     }
-    fetchTitle()
+    fetchHeader()
   }, [])
 
   return (
     <header className="w-full">
       <div className="h-[120px] bg-gradient-to-b from-red-700 to-black flex flex-col items-center justify-center text-white">
         <h1 className="text-3xl font-extrabold">{title}</h1>
-        <p className="italic mt-1">Berani, Jujur, Cerdas, Militan</p>
+        <p className="italic mt-1">{tagline}</p>
         <nav className="mt-3 flex flex-wrap justify-center gap-6 text-sm font-semibold">
           <Link href="/">Beranda</Link>
           <Link href="/berita">Berita</Link>
