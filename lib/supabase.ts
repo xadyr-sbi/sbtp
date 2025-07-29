57275e3fb99efdf520027d008c81f267ad836372
xadyr <xadyran@gmail.com>
2025-07-29 15:09:15 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-// for Server-Side Components / Route Handlers
-export const createClient = () => createSupabaseClient(supabaseUrl, supabaseAnonKey)
-
-// for Client-Side Components
-export const createClientComponent = () => createSupabaseClient(supabaseUrl, supabaseAnonKey)
+export const createClientComponent = () => createClient(supabaseUrl, supabaseAnonKey)
+export const createClientServer = () => createClient(supabaseUrl, supabaseAnonKey)
