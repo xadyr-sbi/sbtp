e3042d55e93c506fd143dd9ea8b262c2f5c50fc7
xadyr <xadyran@gmail.com>
2025-07-29 08:46:19 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300 py-6 mt-12">
-      <div className="max-w-6xl mx-auto px-4 text-center">
-        <p>@2025 SBTP-FSBI</p>
-      </div>
+    <footer className="bg-gray-900 text-gray-300 text-center py-4 mt-8">
+      <p>@2025 SBTP-FSBI</p>
     </footer>
   )
 }
